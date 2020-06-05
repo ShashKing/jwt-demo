679bb86bc4006807dd21df5ee97897d28dbb2803
ShashKing <Shashankk165@gmail.com>
2020-06-05 23:42:36 +0530

    jwt implementation token added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  post 'auth_user' => 'authentication#authenticate_user'
+
+  root to: 'home#index'
   resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
